7198c52867df4c609e27cde2844326b6fb7865a8
kiomnd2 <kiomnd2@naver.com>
2021-03-10 22:46:14 +0900

    my first servlet

---
+++ src/main/java/com/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        System.out.println("hello servlet");
+
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+
+        res.setCharacterEncoding("UTF-8");
+        res.setContentType("text/plain");
+        res.getWriter().println("hello");
+
+
+    }
+}
